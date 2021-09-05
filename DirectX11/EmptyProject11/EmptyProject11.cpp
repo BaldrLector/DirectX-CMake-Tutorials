677c499ecae7b26eb45b589b217c446ec16da609
Guangming Yao <ygm_baldr@163.com>
2021-09-06 01:13:25 +0800

    DXUT

--- DirectX11/EmptyProject11/EmptyProject11.cpp
+++ DirectX11/EmptyProject11/EmptyProject11.cpp
@@ -1,43 +1,34 @@
 //--------------------------------------------------------------------------------------
 // File: EmptyProject11.cpp
 //
-// Empty starting point for new Direct3D 9 and/or Direct3D 11 applications
+// Empty starting point for new Direct3D 11 Win32 desktop applications
 //
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License (MIT).
 //--------------------------------------------------------------------------------------
 #include "DXUT.h"
 
-//--------------------------------------------------------------------------------------
-// Extern declarations 
-//--------------------------------------------------------------------------------------
-extern bool CALLBACK IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
-                                             bool bWindowed, void* pUserContext );
-extern HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice,
-                                            const D3DSURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext );
-extern HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
-                                           void* pUserContext );
-extern void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime,
-                                        void* pUserContext );
-extern void CALLBACK OnD3D9LostDevice( void* pUserContext );
-extern void CALLBACK OnD3D9DestroyDevice( void* pUserContext );
+#pragma warning( disable : 4100 )
+
+using namespace DirectX;
 
 
 //--------------------------------------------------------------------------------------
 // Reject any D3D11 devices that aren't acceptable by returning false
 //--------------------------------------------------------------------------------------
 bool CALLBACK IsD3D11DeviceAcceptable( const CD3D11EnumAdapterInfo *AdapterInfo, UINT Output, const CD3D11EnumDeviceInfo *DeviceInfo,
-                                       DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext )
+                                      DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext )
 {
-    return true;
+  return true;
 }
 
 
 //--------------------------------------------------------------------------------------
-// Called right before creating a D3D9 or D3D11 device, allowing the app to modify the device settings as needed
+// Called right before creating a D3D device, allowing the app to modify the device settings as needed
 //--------------------------------------------------------------------------------------
 bool CALLBACK ModifyDeviceSettings( DXUTDeviceSettings* pDeviceSettings, void* pUserContext )
 {
-    return true;
+  return true;
 }
 
 
@@ -45,9 +36,9 @@ bool CALLBACK ModifyDeviceSettings( DXUTDeviceSettings* pDeviceSettings, void* p
 // Create any D3D11 resources that aren't dependant on the back buffer
 //--------------------------------------------------------------------------------------
 HRESULT CALLBACK OnD3D11CreateDevice( ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc,
-                                      void* pUserContext )
+                                     void* pUserContext )
 {
-    return S_OK;
+  return S_OK;
 }
 
 
@@ -55,9 +46,9 @@ HRESULT CALLBACK OnD3D11CreateDevice( ID3D11Device* pd3dDevice, const DXGI_SURFA
 // Create any D3D11 resources that depend on the back buffer
 //--------------------------------------------------------------------------------------
 HRESULT CALLBACK OnD3D11ResizedSwapChain( ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
-                                          const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext )
+                                         const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext )
 {
-    return S_OK;
+  return S_OK;
 }
 
 
@@ -73,20 +64,19 @@ void CALLBACK OnFrameMove( double fTime, float fElapsedTime, void* pUserContext 
 // Render the scene using the D3D11 device
 //--------------------------------------------------------------------------------------
 void CALLBACK OnD3D11FrameRender( ID3D11Device* pd3dDevice, ID3D11DeviceContext* pd3dImmediateContext,
-                                  double fTime, float fElapsedTime, void* pUserContext )
+                                 double fTime, float fElapsedTime, void* pUserContext )
 {
-    // Clear render target and the depth stencil 
-    float ClearColor[4] = { 0.176f, 0.196f, 0.667f, 0.0f };
+  // Clear render target and the depth stencil
+  auto pRTV = DXUTGetD3D11RenderTargetView();
+  pd3dImmediateContext->ClearRenderTargetView( pRTV, Colors::MidnightBlue );
 
-    ID3D11RenderTargetView* pRTV = DXUTGetD3D11RenderTargetView();
-    ID3D11DepthStencilView* pDSV = DXUTGetD3D11DepthStencilView();
-    pd3dImmediateContext->ClearRenderTargetView( pRTV, ClearColor );
-    pd3dImmediateContext->ClearDepthStencilView( pDSV, D3D11_CLEAR_DEPTH, 1.0, 0 );
+  auto pDSV = DXUTGetD3D11DepthStencilView();
+  pd3dImmediateContext->ClearDepthStencilView( pDSV, D3D11_CLEAR_DEPTH, 1.0, 0 );
 }
 
 
 //--------------------------------------------------------------------------------------
-// Release D3D11 resources created in OnD3D11ResizedSwapChain 
+// Release D3D11 resources created in OnD3D11ResizedSwapChain
 //--------------------------------------------------------------------------------------
 void CALLBACK OnD3D11ReleasingSwapChain( void* pUserContext )
 {
@@ -94,7 +84,7 @@ void CALLBACK OnD3D11ReleasingSwapChain( void* pUserContext )
 
 
 //--------------------------------------------------------------------------------------
-// Release D3D11 resources created in OnD3D11CreateDevice 
+// Release D3D11 resources created in OnD3D11CreateDevice
 //--------------------------------------------------------------------------------------
 void CALLBACK OnD3D11DestroyDevice( void* pUserContext )
 {
@@ -105,9 +95,9 @@ void CALLBACK OnD3D11DestroyDevice( void* pUserContext )
 // Handle messages to the application
 //--------------------------------------------------------------------------------------
 LRESULT CALLBACK MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
-                          bool* pbNoFurtherProcessing, void* pUserContext )
+                         bool* pbNoFurtherProcessing, void* pUserContext )
 {
-    return 0;
+  return 0;
 }
 
 
@@ -123,8 +113,8 @@ void CALLBACK OnKeyboard( UINT nChar, bool bKeyDown, bool bAltDown, void* pUserC
 // Handle mouse button presses
 //--------------------------------------------------------------------------------------
 void CALLBACK OnMouse( bool bLeftButtonDown, bool bRightButtonDown, bool bMiddleButtonDown,
-                       bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
-                       int xPos, int yPos, void* pUserContext )
+                      bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
+                      int xPos, int yPos, void* pUserContext )
 {
 }
 
@@ -134,60 +124,52 @@ void CALLBACK OnMouse( bool bLeftButtonDown, bool bRightButtonDown, bool bMiddle
 //--------------------------------------------------------------------------------------
 bool CALLBACK OnDeviceRemoved( void* pUserContext )
 {
-    return true;
+  return true;
 }
 
 
 //--------------------------------------------------------------------------------------
 // Initialize everything and go into a render loop
 //--------------------------------------------------------------------------------------
-int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
 {
-    // Enable run-time memory check for debug builds.
-#if defined(DEBUG) | defined(_DEBUG)
-    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+  // Enable run-time memory check for debug builds.
+#ifdef _DEBUG
+  _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
 
-    // DXUT will create and use the best device (either D3D9 or D3D11) 
-    // that is available on the system depending on which D3D callbacks are set below
+  // DXUT will create and use the best device
+  // that is available on the system depending on which D3D callbacks are set below
 
-    // Set general DXUT callbacks
-    DXUTSetCallbackFrameMove( OnFrameMove );
-    DXUTSetCallbackKeyboard( OnKeyboard );
-    DXUTSetCallbackMouse( OnMouse );
-    DXUTSetCallbackMsgProc( MsgProc );
-    DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
-    DXUTSetCallbackDeviceRemoved( OnDeviceRemoved );
+  // Set general DXUT callbacks
+  DXUTSetCallbackFrameMove( OnFrameMove );
+  DXUTSetCallbackKeyboard( OnKeyboard );
+  DXUTSetCallbackMouse( OnMouse );
+  DXUTSetCallbackMsgProc( MsgProc );
+  DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
+  DXUTSetCallbackDeviceRemoved( OnDeviceRemoved );
 
-    // Set the D3D9 DXUT callbacks. Remove these sets if the app doesn't need to support D3D9
-    DXUTSetCallbackD3D9DeviceAcceptable( IsD3D9DeviceAcceptable );
-    DXUTSetCallbackD3D9DeviceCreated( OnD3D9CreateDevice );
-    DXUTSetCallbackD3D9DeviceReset( OnD3D9ResetDevice );
-    DXUTSetCallbackD3D9FrameRender( OnD3D9FrameRender );
-    DXUTSetCallbackD3D9DeviceLost( OnD3D9LostDevice );
-    DXUTSetCallbackD3D9DeviceDestroyed( OnD3D9DestroyDevice );
+  // Set the D3D11 DXUT callbacks. Remove these sets if the app doesn't need to support D3D11
+  DXUTSetCallbackD3D11DeviceAcceptable( IsD3D11DeviceAcceptable );
+  DXUTSetCallbackD3D11DeviceCreated( OnD3D11CreateDevice );
+  DXUTSetCallbackD3D11SwapChainResized( OnD3D11ResizedSwapChain );
+  DXUTSetCallbackD3D11FrameRender( OnD3D11FrameRender );
+  DXUTSetCallbackD3D11SwapChainReleasing( OnD3D11ReleasingSwapChain );
+  DXUTSetCallbackD3D11DeviceDestroyed( OnD3D11DestroyDevice );
 
-    // Set the D3D11 DXUT callbacks. Remove these sets if the app doesn't need to support D3D11
-    DXUTSetCallbackD3D11DeviceAcceptable( IsD3D11DeviceAcceptable );
-    DXUTSetCallbackD3D11DeviceCreated( OnD3D11CreateDevice );
-    DXUTSetCallbackD3D11SwapChainResized( OnD3D11ResizedSwapChain );
-    DXUTSetCallbackD3D11FrameRender( OnD3D11FrameRender );
-    DXUTSetCallbackD3D11SwapChainReleasing( OnD3D11ReleasingSwapChain );
-    DXUTSetCallbackD3D11DeviceDestroyed( OnD3D11DestroyDevice );
+  // Perform any application-level initialization here
 
-    // Perform any application-level initialization here
+  DXUTInit( true, true, nullptr ); // Parse the command line, show msgboxes on error, no extra command line params
+  DXUTSetCursorSettings( true, true ); // Show the cursor and clip it when in full screen
+  DXUTCreateWindow( L"EmptyProject11" );
 
-    DXUTInit( true, true, NULL ); // Parse the command line, show msgboxes on error, no extra command line params
-    DXUTSetCursorSettings( true, true ); // Show the cursor and clip it when in full screen
-    DXUTCreateWindow( L"EmptyProject11" );
+  // Only require 10-level hardware or later
+  DXUTCreateDevice( D3D_FEATURE_LEVEL_10_0, true, 800, 600 );
+  DXUTMainLoop(); // Enter into the DXUT ren  der loop
 
-    // Only require 10-level hardware
-    DXUTCreateDevice( D3D_FEATURE_LEVEL_10_0, true, 640, 480 );
-    DXUTMainLoop(); // Enter into the DXUT ren  der loop
+  // Perform any application-level cleanup here
 
-    // Perform any application-level cleanup here
-
-    return DXUTGetExitCode();
+  return DXUTGetExitCode();
 }
 
 
